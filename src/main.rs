b131f8d5b454264b8c29db44b0599787c442da34
lwl <lwl@xkool.xyz>
2021-05-02 17:26:13 +0800

    refactor cli fn

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,25 @@ mod util;
 use util::file::readlines;
 use crate::core::comparing_line::ComparingLine;
 use crate::display::print_comparing_lines;
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 fn main() {
-    let matches = App::new("rdiff")
+    let matches = get_args();
+
+    let file1 = matches.value_of("File1").unwrap_or("");
+    let file2 = matches.value_of("File2").unwrap_or("");
+    let width = matches.value_of("width").unwrap_or("50").parse::<usize>().unwrap_or(50);
+
+    let lines1: Vec<String> = readlines(file1);
+    let lines2: Vec<String> = readlines(file2);
+
+    let comparing_lines = ComparingLine::from(lines1, lines2, Option::Some(width), Option::Some('|'));
+
+    print_comparing_lines(&comparing_lines);
+}
+
+fn get_args() -> ArgMatches {
+    App::new("rdiff")
         .version("v1.0")
         .author("written by: Pye<pyeprog@foxmail.com>")
         .about("description: a simple diff in rust")
@@ -27,16 +42,5 @@ fn main() {
             .help("set the virtual win width")
             .default_value("50")
             .takes_value(true))
-        .get_matches();
-
-    let file1 = matches.value_of("File1").unwrap_or("");
-    let file2 = matches.value_of("File2").unwrap_or("");
-    let width = matches.value_of("width").unwrap_or("50").parse::<usize>().unwrap_or(50);
-
-    let lines1: Vec<String> = readlines(file1);
-    let lines2: Vec<String> = readlines(file2);
-
-    let comparing_lines = ComparingLine::from(lines1, lines2, Option::Some(width), Option::Some('|'));
-
-    print_comparing_lines(&comparing_lines);
+        .get_matches()
 }
